78d9f1819ffb54be990e266e614f983f5bccbefc
Etay Meiri <etay.meiri@gmail.com>
2020-10-06 22:09:10 +0300

    Set target name to basename if there is only one file

--- bts.py
+++ bts.py
@@ -3,11 +3,15 @@
 import glob
 import sys
 import subprocess
+import os
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         cpp_files = glob.glob("*.cpp")
+        target_name = "runme"
+        if len(cpp_files) == 1:
+            target_name = os.path.basename(cpp_files[0][:-4])
         print(cpp_files)
-        cmd = ["g++", " ".join(cpp_files)]
+        cmd = ["g++", " ".join(cpp_files), "-o", target_name]
         print(cmd)
         output = subprocess.check_output(cmd)
